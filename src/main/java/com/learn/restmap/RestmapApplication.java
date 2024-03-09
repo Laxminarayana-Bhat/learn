8249ec2f43b126a80118797cdc460aa376c98206
paul <paulwalker787898@gmail.com>
2024-03-09 23:29:22 +0530

    latest commit

---
+++ src/main/java/com/learn/restmap/RestmapApplication.java
@@ -0,0 +1,13 @@
+package com.learn.restmap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestmapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestmapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/learn/restmap/RestmapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learn.restmap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestmapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
